bd93bcd5238beec2e8ab855311d68f69712bce22
niices <niices@outlook.com>
2016-08-16 10:36:52 +0800

    fix some bugs

--- themes/next/source/js/important.js
+++ themes/next/source/js/important.js
@@ -1,13 +1,6 @@
-// $(document).ready(function() {
-// 	$('.ds-icon-heart').attr("rel", "like");
-//     $('.ds-icon-heart').click(function() {
-//         $(this).css("background-position", "")
-//         var D = $(this).attr("rel");
-//         if (D === 'like') {
-//             $(this).addClass("heartAnimation").attr("rel", "unlike");
-//         } else {
-//             $(this).removeClass("heartAnimation").attr("rel", "like");
-//             $(this).css("background-position", "left");
-//         }
-//     });
-// });
+$(document).ready(function() {
+	function setComments(){
+        $(".ds-time").show();
+    }
+    setTimeout(setComments,2000)
+});
